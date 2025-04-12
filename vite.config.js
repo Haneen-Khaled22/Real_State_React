d30b7f0e14a08c6cc5772b03880242c4901a15cb
Haneen-Khaled22 <haneenk385@gmail.com>
2025-04-12 21:04:01 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/realstatereact"
+  base :"/Real_State_React"
+  
 
 })
